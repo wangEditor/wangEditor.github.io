ec7d5532eeecca5a60a99be5f7fd6db2c0b0b153
wangfupeng <wangfupeng1988@163.com>
2015-12-05 12:00:31 +0800

    test

--- wangEditor-mobile/dist/js/wangEditor-mobile.js
+++ wangEditor-mobile/dist/js/wangEditor-mobile.js
@@ -99,17 +99,19 @@
 	// console.log 函数
 	E.log = function (a, b) {
 		if (window.console) {
-			if (b) {
-				console.log(a, b);
-			} else {
-				console.log(a);
-			}
+			if (b) { console.log(a, b); } else { console.log(a); }
 		} else {
 			alert(a);
-
-			if (b) {
-				alert(b);
-			}
+			if (b) { alert(b); }
+		}
+	};
+	// console.warn 函数
+	E.warn = function (a, b) {
+		if (window.console) {
+			if (b) { console.warn(a, b); } else { console.warn(a); }
+		} else {
+			alert(a);
+			if (b) { alert(b); }
 		}
 	};
 
@@ -150,8 +152,9 @@ window.___E_mod(function (E, $) {
 				'color',
 				'quote',
 				'list',
-				'happy',
-				'check'
+				'img',
+				'happy'//,
+				//'check'
 			],
 
 			// 表情图标配置
@@ -176,7 +179,13 @@ window.___E_mod(function (E, $) {
 				'http://wangeditor.github.io/expressions/18.gif',
 				'http://wangeditor.github.io/expressions/19.gif',
 				'http://wangeditor.github.io/expressions/20.gif'
-			]
+			],
+
+			// 上传图片
+			uploadImgUrl: '/upload',
+
+			// 测试地址（在测试地址，编辑器会主动输出一些console.log信息）
+			testHostname: 'localhost'
 		};
 		
 	};
@@ -254,6 +263,7 @@ window.___E_mod(function (E, $) {
 		self.addMenuList('list');
 		self.addMenuCheck('check');
 		self.addMenuHappy('happy');
+		self.addMenuImg('img');
 	};
 
 });
@@ -617,6 +627,14 @@ window.___E_mod(function (E, $) {
 		var happyUrlArr = self.config.happy;
 		var $txt = self.$txt;
 
+		// 提醒，不要使用编辑器自带的表情
+		if (happyUrlArr.length > 0) {
+			if (happyUrlArr[0].indexOf('http://wangeditor.github.io/expressions') === 0) {
+				E.warn('正在使用wangEdior提供的免费表情图标，它们将从 github 下载，速度很慢！！！');
+				E.warn('建议将表情图标重新配置，请参见文档说明');
+			}
+		}
+
 		menus[menuId] = {
 			// 选中状态
 			selected: false,
@@ -666,20 +684,28 @@ window.___E_mod(function (E, $) {
 					// 隐藏 modal
 					menuData.hideModal();
 				});
+			},
+
+			// 显示 $modal
+			showModal: function () {
+				var menuData = this;
+
+				// 显示 modal
+				menuData.$modal.show();
 
 				// 点击编辑区域隐藏modal
-				$txt.on('singleTap', function (e) {
-					if (self.checkTapTime() === false) {
-						return;
-					}
+				var hideModalFn = function (e) {
 					menuData.hideModal();
-				});
+
+					// 隐藏modal之后，接着取消事件绑定
+					$txt.off('singleTap', hideModalFn);
+				};
+
+				// 绑定事件
+				$txt.on('singleTap', hideModalFn);
 			},
 
-			// 显示、隐藏 $modal
-			showModal: function () {
-				this.$modal.show();
-			},
+			// 隐藏 $modal
 			hideModal: function () {
 				this.$modal.hide();
 			},
@@ -729,6 +755,148 @@ window.___E_mod(function (E, $) {
 		};	
 	};
 
+});
+// 上传图片
+window.___E_mod(function (E, $) {
+	E.fn.addMenuImg = function (menuId) {
+		var self = this;
+		var $body = self.$body;
+		var menus = self.menus;
+		var config = self.config;
+		var uploadImgUrl = config.uploadImgUrl || '';
+		var testHostname = config.testHostname || 'localhost';
+		var idDebugger = testHostname === location.hostname;
+
+		if (uploadImgUrl === '') {
+			alert(
+				'没有配置 uploadImgUrl ，wangEditor 将无法上传图片。\n\n' + 
+				'想要配置上传图片，请参见文档说明。\n\n' + 
+				'不想要图片上传，可通过配置 menus 隐藏该功能。'
+			);
+		}
+
+		// 针对 test 地址，打印信息
+		function log(info) {
+			if (!idDebugger) {
+				return;
+			}
+			E.log(info);
+		}
+
+		// 用随机数生成 iframe formid 和 input 的 id
+		var iframeId = 'iframe' + Math.random().toString().slice(2);
+		var formId = 'form' + Math.random().toString().slice(2);
+		var inputFileId = 'inputfile' + Math.random().toString().slice(2);
+
+		menus[menuId] = {
+			// 选中状态
+			selected: false,
+
+			// 触发器
+			$trigger: $('<a href="#"><i class="icon-wangEditor-m-picture"></i></a>'),
+			// 包裹触发器的容器
+			$wrap: $('<div class="item"</div>'),
+
+			// 渲染 iframe
+			renderIframe: function () {
+				// 拼接html
+				var iframeHtml = '<iframe id="' + iframeId + '" name="' + iframeId + '" style="display:none; width:0; height:0;"></iframe>';
+				var $container = $('<div style="display:none;"></div>');
+				$container.html(iframeHtml);
+
+				// 渲染到页面中
+				$body.append($container);
+
+				// 绑定 iframe 的 onload 事件
+				var iframeElem = $('#' + iframeId).get(0);
+				iframeElem.onload = function () {
+					var result = iframeElem.contentDocument.body.innerHTML;
+					if (result === 'error') {
+						log('服务器端返回了数据，很遗憾返回的是 error');
+						log('请关注弹出的错误提示，或者去服务器后台跟踪代码，排查问题');
+						log('结束...');
+						
+						alert('上传错误');
+						return;
+					}
+
+					log('成功接收到服务器端返回的数据');
+					log('服务器端返回的数据是：' + result);
+					log('请确认以上数据是否是一个 image 的 url 地址，如果不是将无法显示 image');
+					log('即将显示 image');
+
+					// 执行插入命令
+					self.command('insertimage', false, result);
+
+					log('结束...');
+				};
+			},
+
+			// 渲染 form
+			renderForm: function () {
+				var fromHtml = [
+					'<form id="' + formId + '" target="' + iframeId + '"',
+					'    action="' + uploadImgUrl + '"',
+					'    enctype="multipart/form-data"',
+					'    method="post">',
+					// 下一句代码的 name  值必须为 wangEditorMobileFile，和后台代码一致
+					'    <input id="' + inputFileId + '" name="wangEditorMobileFile" type="file" accept="image/*" capture="camera">',
+					'</form>'
+				].join('');
+
+				var $container = $('<div style="display:none;"></div>');
+				$container.html(fromHtml);
+
+				// 渲染到页面中
+				$body.append($container);
+			},
+
+
+			// 绑定菜单事件
+			bindEvent: function (editor) {
+				var menuData = this;
+				var $trigger = menuData.$trigger;
+
+				// 将 iframe 和 form 渲染到页面上
+				menuData.renderIframe();
+				menuData.renderForm();
+
+				// 获取 form input-file 对象
+				var $inputFlie = $('#' + inputFileId);
+				var $form = $('#' + formId);
+
+				// input 有文件选中时，提交 form
+				$inputFlie.on('change', function (e) {
+					$form.submit();
+					log('已经选中文件，并提交了 form');
+					log('如果接下来得不到 log 信息，则证明服务器端没有返回数据');
+				});
+
+				// 点击菜单，触发 input 事件
+				$trigger.on('singleTap', function (e) {
+					if (self.checkTapTime() === false) {
+						return;
+					}
+
+					if (uploadImgUrl === '') {
+						// 没有配置上传图片的url
+						return;
+					}
+
+					function fn() {
+						$inputFlie.trigger('click');
+					}
+
+					self.customCommand(true, fn, e);
+				});
+			},
+
+			// 更新样式
+			updateStyle: function () {
+				// 暂时不需要
+			}
+		};
+	};
 });
 // 渲染编辑器区域
 window.___E_mod(function (E, $) {
@@ -1330,6 +1498,15 @@ window.___E_mod(function (E, $) {
 		var currentWrapRange = self.currentWrapRange();
 		var $txt = self.$txt;
 
+		/*
+			isRestoreNoWrapSelection 参数的作用：
+			1. 有些 command 是需要选中整个外围选区再进行操作的，一般是修改样式，例如加粗。
+			   针对加粗这种样式操作，如果不默认选中一个选区，是看不到任何效果的。
+			2. 但是有些 command 一定不能选中外围选区，一般是插入操作，例如插入图片。
+			   如果选中了一段区域，再执行插入图片，插入图片之后，刚才的那段选区就没有了。
+
+			因此，isRestoreNoWrapSelection 的作用就是来判断，是否要选中外围选区。
+		*/
 		if (isRestoreNoWrapSelection) {
 			// 恢复选区（非整个外围选区）
 			self.restoreSelection(currentRange);

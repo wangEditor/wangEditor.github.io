aaf7d3744faf4bfead6d8bb7c79480906b0062e4
wangfupeng <wangfupeng1988@163.com>
2016-04-03 18:10:53 +0800

    v2.1.0

--- index.js
+++ index.js
@@ -4,41 +4,71 @@ $(function (){
 
     editor.config.uploadImgUrl = '/upload';
 
-    // 设置表情
+    // 表情
     editor.config.emotions = {
         'default': {
             title: '默认',
-            size: 18,
-            imgs: [
-                './static/emotions/default/1.gif',
-                './static/emotions/default/2.gif',
-                './static/emotions/default/3.gif',
-                './static/emotions/default/4.gif',
-                './static/emotions/default/5.gif',
-                './static/emotions/default/6.gif',
-                './static/emotions/default/7.gif',
-                './static/emotions/default/8.gif',
-                './static/emotions/default/9.gif',
-                './static/emotions/default/10.gif',
-                './static/emotions/default/11.gif',
-                './static/emotions/default/12.gif',
-                './static/emotions/default/13.gif',
-                './static/emotions/default/14.gif',
-                './static/emotions/default/15.gif',
-                './static/emotions/default/16.gif',
-                './static/emotions/default/17.gif',
-                './static/emotions/default/18.gif',
-                './static/emotions/default/19.gif',
-                './static/emotions/default/20.gif'
-            ]
+            data: './wangEditor/test/emotions.data'
         },
-        'jinxing': {
-            title: '金星',
-            size: 50,
-            imgs: [
-                './static/emotions/jinxing/1.gif',
-                './static/emotions/jinxing/2.gif',
-                './static/emotions/jinxing/6.gif'
+        'weibo': {
+            title: '微博表情',
+            data: [
+                {
+                    icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/7a/shenshou_thumb.gif',
+                    value: '[草泥马]'    
+                },
+                {
+                    icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/60/horse2_thumb.gif',
+                    value: '[神马]'    
+                },
+                {
+                    icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/bc/fuyun_thumb.gif',
+                    value: '[浮云]'    
+                },
+                {
+                    icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/c9/geili_thumb.gif',
+                    value: '[给力]'    
+                },
+                {
+                    icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/f2/wg_thumb.gif',
+                    value: '[围观]'    
+                },
+                {
+                    icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/70/vw_thumb.gif',
+                    value: '[威武]'
+                },
+                {
+                    icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/6e/panda_thumb.gif',
+                    value: '[熊猫]'
+                },
+                {
+                    icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/81/rabbit_thumb.gif',
+                    value: '[兔子]'
+                },
+                {
+                    icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/bc/otm_thumb.gif',
+                    value: '[奥特曼]'
+                },
+                {
+                    icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/15/j_thumb.gif',
+                    value: '[囧]'
+                },
+                {
+                    icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/89/hufen_thumb.gif',
+                    value: '[互粉]'
+                },
+                {
+                    icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/c4/liwu_thumb.gif',
+                    value: '[礼物]'
+                },
+                {
+                    icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/ac/smilea_thumb.gif',
+                    value: '[呵呵]'
+                },
+                {
+                    icon: 'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/0b/tootha_thumb.gif',
+                    value: '[哈哈]'
+                }
             ]
         }
     };

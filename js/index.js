fa627be2f603d2dc16975a02e39803a13f452be9
hahaaha <390519764@qq.com>
2021-02-23 13:54:30 +0800

    feature: 英文文档部分设置i18n

--- js/index.js
+++ js/index.js
@@ -6,4 +6,15 @@ $(function (){
     editor.config.height = 800
 
     editor.create()
+
+    var editor_en = new E('#div-demo1')
+
+    editor_en.config.uploadImgShowBase64 = true
+    editor_en.config.height = 800
+    
+    editor_en.config.lang = 'en'
+    editor_en.i18next = window.i18next
+
+    editor_en.create()
+    
 });
